75cb1597b907189b9f0861f71cad1c30d4d84e76
Justin Poliachik <jpoliachik@gmail.com>
2025-03-20 16:35:31 -0400

    use modalscreenwrapper

--- app/green/index.tsx
+++ app/green/index.tsx
@@ -2,16 +2,17 @@ import { router } from "expo-router";
 import { Button, View } from "react-native";
 import * as React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { ModalScreenWrapper } from "@/components/ModalScreenWrapper";
 
 export default function GreenScreen() {
   const insets = useSafeAreaInsets();
   return (
-    <>
+    <ModalScreenWrapper>
       <View style={{ flex: 1, paddingBottom: insets.bottom }}>
         <Button title="Green / Modal " onPress={() => router.navigate("/green/modal")} />
         <View style={{ flex: 1, width: 50, backgroundColor: "green" }} />
         <View style={{ height: 50, width: "100%", backgroundColor: "darkgreen" }} />
       </View>
-    </>
+    </ModalScreenWrapper>
   );
 }
